2e599bbe9c854240f3eb87a8d3dd6dab14a4c70a
jeffgchapin <jeffgchapin@gmail.com>
2015-02-08 19:09:21 -0800

    1st

---
+++ js/send.js
@@ -0,0 +1,19 @@
+﻿function send(from_bld_id) {
+  var view_count = 3;
+  ajax_get_all_out_boxes(function (response) {
+    if (response.d.length > 2) {
+      $("#destination_list").empty();
+      var data = $.parseJSON(response.d);
+      $.each(data, function (i, item) {
+        $("#destination_list").append("<li>" +
+          "<a data-theme='a' id='li" + i + "' data-transition='slide' href='mailbox.htm?to_bld_id=" + item.to_bld_id + "'>" +
+            item.building +
+            "<span class='ui-li-count'>" + item.mail_count + "</span>" +
+          "</a>" +
+          "</li>"
+        );
+      });
+      $("#destination_list").listview("refresh");
+    }
+  });
+}
